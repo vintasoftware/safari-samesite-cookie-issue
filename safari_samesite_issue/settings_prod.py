1a18e675ca58bdd337f6616053ec19a221e7439e
Flávio Juvenal <flaviojuvenal@gmail.com>
2019-03-29 15:52:33 -0300

    Env vars in settings_prod.py for samesite attribute

--- safari_samesite_issue/settings_prod.py
+++ safari_samesite_issue/settings_prod.py
@@ -7,4 +7,7 @@ DEBUG = False
 import django_heroku
 django_heroku.settings(locals())
 
+CSRF_COOKIE_SAMESITE = os.environ.get('CSRF_COOKIE_SAMESITE', None)
+SESSION_COOKIE_SAMESITE = os.environ.get('SESSION_COOKIE_SAMESITE', None)
+
 REDIRECT_TARGET_URL = os.environ['REDIRECT_TARGET_URL']
